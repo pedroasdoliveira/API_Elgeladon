c081dbd4b1542e626d0b4d66ea1f417bfbc359e1
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-04-13 00:35:37 -0300

    firts commit

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+const baseURL = "http://localhost:3001/paletas"
+
+async function findAllPaletas() {
+    const response = await fetch(`${baseURL}/todas-paletas`);
+
+    const paletas = await response.json();
+
+    paletas.forEach(function (paleta) {
+        document.querySelector('#paletaList').insertAdjacentHTML('beforeend',
+            `
+        <div class="PaletaListaItem">
+            <div>
+            <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
+            <div class="PaletaListaItem__preco">R$ ${paleta.preco}</div>
+            <div class="PaletaListaItem__descricao">${paleta.descricao}</div>
+            </div>
+            <img class="PaletaListaItem__foto" src="${paleta.foto}" alt="Paleta de ${paleta.sabor}"/>
+        </div>
+        `)
+    });
+}
+
+async function findByIdPaletas() {
+    const id = document.querySelector('#idPaleta').value
+
+    const response = await fetch(`${baseURL}/paleta/${id}`);
+    const paleta = await response.json();
+
+    const paletaEscolhidaDiv = document.querySelector('#paletaEscolhida');
+
+    paletaEscolhidaDiv.innerHTML = `
+    <div class="PaletaCardItem">
+        <div>
+        <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
+        <div class="PaletaCardItem__preco">R$ ${paleta.preco}</div>
+        <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
+        </div>
+        <img class="PaletaCardItem__foto" src="${paleta.foto}" alt="Paleta de ${paleta.sabor}"/>
+    </div>
+    `
+}
+
+findAllPaletas();
